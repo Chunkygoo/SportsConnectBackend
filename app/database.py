8df45868e4bc53b53f6c1558f2a121b7a630e837
CalvinTai0402 <calvintai0402@gmail.com>
2022-07-23 15:27:51 -0700

    added inherit cache

--- app/database.py
+++ app/database.py
@@ -1,5 +1,9 @@
 from sqlmodel import Session, create_engine
 from .config import settings
+from sqlmodel.sql.expression import Select, SelectOfScalar
+
+SelectOfScalar.inherit_cache = True
+Select.inherit_cache = True
 
 SQLMODEL_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 engine = create_engine(SQLMODEL_DATABASE_URL)
